2e00e36acf6a8fa017b7a2127a4269733e80c88e
mbahoshy <mbahoshy@gmail.com>
2013-09-27 11:25:13 -0700

    ready to deploy

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,28 @@
+$(document).ready(function(){
+
+	$('#add_button').click(addUser); //assign click event to add button
+	edit_open = 'false'; //edit_open prevents multiple editView from being instantiated
+	ulist.reset(defaultUsers); //populate with default users
+
+});
+
+
+//called when add_button is clicked
 function addUser () {
-	
+	//assign variables from input fields
 	newNameFirst = $('#add_user .nameFirst input').val();
 	newNameLast = $('#add_user .nameLast input').val();
 	newEmail = $('#add_user .email input').val();
 
 	if (newNameFirst && newNameLast && newEmail) {
+		//creates new User instance based on input data
 		new User({nameFirst:newNameFirst, nameLast:newNameLast, email:newEmail});
+
+		//clears text from input fields
 		$('#add_user .nameFirst input').val("");
 		$('#add_user .nameLast input').val("");
 		$('#add_user .email input').val("");
 	} else {
-		alert('please enter all required fields');
+		alert('Please enter all required fields');
 	}
 }
--- js/usermodel.js
+++ js/usermodel.js
@@ -1,9 +1,9 @@
+//creates user model
 var User = Backbone.Model.extend({
-	className: 'userclass',
-	urlRoot: '/userlist',
 	initialize : function () {
-		ulist.add(this);
+		//automatically instantiate new view when model is created
 		new userView ({model: this});
+		ulist.add(this); //add model to collection
 	},
 	defaults: {
 		'nameFirst' : '',
@@ -12,33 +12,31 @@ var User = Backbone.Model.extend({
 	}
 });
 
-var UsersList = Backbone.Collection.extend({
-	model: User
-});
-
-var ulist = new UsersList();
-
-
+//creates view used to display users
 var userView = Backbone.View.extend({
 	tagName: 'tr',
 	events: {
 		"click .delete": "destroyUser",
-		"click .edit" : "alertedit"
+		"click .edit" : "editUser"
 	},
 	initialize: function () {
+		//assign event listeners to render userView on changes to model, or remove view on model destroy
 		this.model.on('change', this.render, this);
 		this.model.on('destroy', this.remove, this);
 		this.render();
 	},
-	destroyUser : function (e) {
+	//destroy user method
+	destroyUser : function () {
 		this.model.destroy();
 	},
-	alertedit : function (e) {
+	//edit user method
+	editUser : function () {
 		if (edit_open == 'false') {
-			new editView ({model: this.model});
-			edit_open = 'true';
+			new editView ({model: this.model}); //instantiates new editView
+			edit_open = 'true'; //prevents multiple 'edit_users' from being instantiated
 		}	
 	},
+	//renders html for userView
 	render : function () {
 		var html = "<td>" + this.model.get('nameFirst') + "</td><td>" + this.model.get('nameLast') + "</td><td>" + this.model.get('email') + "</td><td class='icons'><img class='edit' src='images/edit.png'	/><img class='delete' src='images/delete.png'	/></td>";
 		this.$el.html(html);
@@ -46,42 +44,63 @@ var userView = Backbone.View.extend({
 	}
 });
 
+//creates view to edit users
 var editView = Backbone.View.extend({
-	//tagName: 'tr',
 	initialize: function () {
-		this.model.on('destroy', this.remove, this);
+		//automatically render when view is initialized
 		this.render();
 	},
 	events: {
 		"click #edit_button" : "updateUser"
 	},
-	updateUser: function (e) {
+	//update user method
+	updateUser: function () {
+		//assign varaibles to input values
 		newNameFirst = $('#edit_user .nameFirst input').val();
 		newNameLast = $('#edit_user .nameLast input').val();
 		newEmail = $('#edit_user .email input').val();
-		this.model.set({nameFirst: newNameFirst, nameLast: newNameLast, email:newEmail});
-		$('#edit_user').fadeToggle();
-		this.remove();
-		edit_open = 'false';
+
+		if (newNameFirst && newNameLast && newEmail) {
+			//update model with new information
+			this.model.set({nameFirst: newNameFirst, nameLast: newNameLast, email:newEmail});
+			
+			$('#edit_user').fadeToggle(); //fade out edit_user div
+			this.remove(); //destroy editView
+			edit_open = 'false'; //allow another editView instance to be created
+		} else {
+			alert('Field may not be left blank');
+		}
 
 	},
 	render : function () {
+		//assign variables to current model values
 		editNameFirst = this.model.get('nameFirst');
 		editNameLast = this.model.get('nameLast');
 		editEmail = this.model.get('email');
+
+		//render html
 		var html = "<span class='nameFirst'>First Name:<input value='" + editNameFirst + "' type='text'></span><span class='nameLast'>Last Name: <input value='" + editNameLast + "' type='text'></span><span class='email'>Email: <input value='" + editEmail + "' type='text'></span><div id='edit_button'>Edit User</div>";
 		this.$el.html(html);
+
+		//append html to proper div, and fade div display
 		$('#edit_user').append(this.el);
 		$('#edit_user').fadeToggle();
 	}
 });
 
-new User({nameFirst:'Matthew', nameLast:'Ross', email:'mbahoshy@gmail.com'});
-new User({nameFirst:'Joe', nameLast:'Ross', email:'joe@gmail.com'});
+//creates a new collection
+var UserList = Backbone.Collection.extend({
+	model:User
+});
 
+//instantiates new collection
+var ulist = new UserList();
 
-/*
-var newView = new userView({model: newUser});
-var newView2 = new userView({model: newUser2});
-*/
-//alert(newUser.get('nameFirst'));
+//data for defaultUsers (populated on document.ready)
+var defaultUsers = [
+	{nameFirst:'Matthew', nameLast:'Ross', email:'mbahoshy@gmail.com'},
+	{nameFirst:'Mark', nameLast:'Twain', email:'mtwain@hotmail.com'},
+	{nameFirst:'Jason', nameLast:'Bourne', email:'johndoe@yahoo.com'},
+	{nameFirst:'Victoria', nameLast:'Secret', email:'vs@gmail.com'},
+	{nameFirst:'Willie', nameLast:'Nelson', email:'wnelson@yahoo.com'}
+];
